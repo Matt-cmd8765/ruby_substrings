bb61c2c870be9df880dafefa09e93d2d3ae504b3
Matt Marini <mattmarini8765@gmail.com>
2023-04-25 21:21:25 +1000

    More progress, have to eliminate doubling up hash and array

--- substrings.rb
+++ substrings.rb
@@ -1,14 +1,29 @@
-dictionary = ["below","down","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+#word.downcase.split(/\W+/)
 
 def substrings(word, array)
-    splits = word.split(' ')
-    splits.each do 
-    add = array.select { |substring| substring == word}
-    new_hash = add.reduce(Hash.new(0)) do |key, value|
-        key[value] += 1
-        key
+    splits = word.downcase.split(/\W+/)
+    #puts splits[0].include? "low"
+    #puts splits
+    new_array = []
+    splits.each do |subword|
+        array.each do |thing|
+            if subword.include? thing
+                new_array.push(thing)
+            end
+        end
+        add = array.select { |substring| substring == subword}
+        add.each do |new_word|
+            new_array.push(new_word)
+        end
+    end
+    p new_array
+    new_hash = new_array.reduce(Hash.new(0)) do |count, string| 
+        count[string] += 1
+        count
     end
     puts new_hash
 end
 
-substrings("down below", dictionary)
+substrings("below", dictionary)
